1c38bb415e3c990e3c112db22305f073068c0417
vinith2k <vinithk121@gmail.com>
2023-01-20 12:32:34 +0530

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<div class="container">
+	<hgroup>
+		<h1>
+			<a href="/"> SvelteKit & Prisma 🔥</a>
+		</h1>
+
+		<h2> CRUD application with SvelteKit & Prisma.</h2>
+	</hgroup>
+	<slot />
+</div>
+
