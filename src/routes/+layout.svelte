51b161ace36541164b2325282326dac8c56c11d4
vinith2k <vinithk121@gmail.com>
2023-03-15 11:22:06 +0530

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,43 @@
+<script>
+	import { onMount } from 'svelte'
+	import { pwaInfo } from 'virtual:pwa-info'
+
+	
+	let ReloadPrompt
+  
+	onMount(async () => {
+	  if (pwaInfo) {
+		const { registerSW } = await import('virtual:pwa-register')
+		registerSW({
+		  immediate: true,
+		  onRegistered(r) {
+			
+			console.log(`SW Registered: ${r}`)
+		  },
+		  onRegisterError(error) {
+			console.log('SW registration error', error)
+		  }
+		})
+	  }
+	});
+
+	onMount(async () => {
+    pwaInfo && (ReloadPrompt = (await import('$lib/ReloadPrompt.svelte')).default)
+  });
+	
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+  </script>
+  
+  <svelte:head>
+	  {@html webManifest}
+  </svelte:head>
+  
+
+  {#if ReloadPrompt}
+  <svelte:component this={ReloadPrompt} />
+{/if}
+
+
 <div class="container">
 	<hgroup>
 		<h1>
